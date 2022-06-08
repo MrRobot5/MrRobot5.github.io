459fb0c089f35f41939a17065d1e6a5dc21b6b31
yangpan23 <yangpan3@jd.com>
2022-06-08 18:08:00 +0800

    docs: 增加shell 脚本

---
+++ script/mrrobot-git-update.sh
@@ -0,0 +1,27 @@
+
+cd D:/yangpan3/MrRobot5.github.io
+pwd
+gpp
+
+cd D:/workspace-experiment/quotation-calculator
+pwd
+gpp 
+
+cd D:/workspace-experiment/sample-base-more
+pwd
+gpp 
+
+cd D:/workspace-experiment/chocolate-factory
+pwd
+gpp 
+
+cd D:/workspace-experiment/mybatis-sample
+pwd
+gpp
+
+cd D:/软件/idea
+pwd
+gpp
+
+echo 'await'
+sleep 1m
